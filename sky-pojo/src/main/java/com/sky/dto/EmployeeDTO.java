25ac91a95d1e0966b4a47d9853f6a2f705e69766
yukki <328763931@qq.com>
2025-03-20 21:51:01 +0800

    1.创建系统数据库，以及相关的用户、菜品、员工表。 2.明文密码md5加密 3.Swagger依赖注入，以及使用Swagger注解描述测试接口

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,32 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("员工数据传递对象")
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty("员工id")
     private Long id;
 
+    @ApiModelProperty("员工账户")
     private String username;
 
+    @ApiModelProperty("员工名字")
     private String name;
 
+    @ApiModelProperty("员工手机号")
     private String phone;
 
+    @ApiModelProperty("员工性别")
     private String sex;
 
+    @ApiModelProperty("员工身份证号")
     private String idNumber;
 
 }
