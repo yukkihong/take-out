16dab49145acca1da1c83b222e9d5288873d1f29
yukki <328763931@qq.com>
2025-03-28 16:10:13 +0800

    下单业务功能实现。

---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface OrderDetailMapper {
+    /**
+     * 插入订单数据
+     * @param orderDetailList
+     */
+    void insertBatch(List<OrderDetail> orderDetailList);
+
+
+}
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -10,6 +10,7 @@ public interface UserMapper {
     @Select("select * from sky_take_out.user where openid = #{openId}")
     User getUserByOpenId(String openId);
 
-
+    @Select("select  * from sky_take_out.user where  id = #{id}")
+    User getById(long id);
     void insert(User user);
 }
